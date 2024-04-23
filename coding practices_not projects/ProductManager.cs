35984c33a349186cf98eab05b99383460bc45072
salimdev3469 <157348323+salimdev3469@users.noreply.github.com>
2024-04-23 08:19:17 +0300

    some codes to understand c#

---
+++ coding practices_not projects/ProductManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class ProductManager
+    {
+        public void add()
+        {
+            Console.WriteLine("product added");
+        }
+        public void update()
+        {
+            Console.WriteLine("product updated");
+        }
+    }
+}
---
+++ coding practices_not projects/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager can=new CustomerManager();
+            can.add();
+            can.update();
+
+            ProductManager canan=new ProductManager();
+            canan.add();
+            canan.update();
+            Customer hakan=new Customer();
+            hakan.City = "Bursa";
+            hakan.Id = 2;
+            hakan.Firstname = "Hakan";
+            hakan.Lastname = "Tasdelen";
+
+            Customer mazlum = new Customer
+            {
+                Id = 1,City = "Elazig",Firstname = "Mazlum",Lastname = "Gün"
+            };
+            Console.WriteLine(mazlum.Firstname);
+
+            Console.ReadLine();
+        }
+    }
+
+    
+    
+}
